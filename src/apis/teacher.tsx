7d8842e6090454e7eb4d866736fec57785659b9c
yuunik <yuunik_zhou@163.com>
2024-06-05 22:41:26 +0800

    fix: 讲师列表 - 条件查询bug修复

--- src/apis/teacher.tsx
+++ src/apis/teacher.tsx
@@ -10,18 +10,27 @@ const base_url = '/eduservice/teacher'
 
 // 请求地址管理
 enum TeacherApi {
-  // 获取讲师列表
-  GET_TEACHER_LIST = `${base_url}/pageTeacherListByCondition/`,
+  // 分页查询讲师列表
+  PAGE_TEACHER_LIST = `${base_url}/pageTeacherList/`,
+  // 根据条件分页查询讲师列表
+  PAGE_TEACHER_LIST_BY_CONDITION = `${base_url}/pageTeacherListByCondition/`,
 }
 
 // 分页查询讲师列表
+export const pageTeacherListAPI = ({ current, pageSize }: TeacherParams) =>
+  request<ResType<PageRes<Teacher[]>>>({
+    url: TeacherApi.PAGE_TEACHER_LIST + `${current}/${pageSize}`,
+    method: 'GET',
+  })
+
+// 根据条件分页查询讲师列表
 export const getTeacherListByConditionAPI = ({
   current,
   pageSize,
   teacherQuery,
 }: TeacherParams) =>
   request<ResType<PageRes<Teacher[]>>>({
-    url: TeacherApi.GET_TEACHER_LIST + `${current}/${pageSize}`,
+    url: TeacherApi.PAGE_TEACHER_LIST_BY_CONDITION + `${current}/${pageSize}`,
     method: 'POST',
     // data 会以 json 格式传输数据
     data: teacherQuery,
--- src/pages/teacher/list/index.tsx
+++ src/pages/teacher/list/index.tsx
@@ -16,7 +16,9 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
 
 const List = () => {
+  // dayjs 使用中文
   dayjs.locale('zh-cn')
+
   // 讲师列表
   const [teacherList, setTeacherList] = useState<Teacher[]>([])
 
@@ -26,6 +28,7 @@ const List = () => {
     pageSize: 3,
   })
 
+  // 讲师查询条件
   const [teacherQuery, setTeacherQuery] = useState<TeacherQuery>({
     name: '',
     level: 0,
@@ -36,8 +39,8 @@ const List = () => {
   // 总页数
   const [total, setTotal] = useState<number>(0)
 
-  // 获取讲师列表
-  const getTeacherList = async () => {
+  // 根据条件分页查询讲师列表
+  const pageTeacherListByCondition = async () => {
     const {
       data: {
         code,
@@ -45,7 +48,7 @@ const List = () => {
       },
     } = await getTeacherListByConditionAPI({
       ...pageData,
-      teacherQuery: teacherQuery,
+      teacherQuery,
     })
     if (code === 20000) {
       // 更新讲师列表
@@ -55,9 +58,9 @@ const List = () => {
     }
   }
 
-  // 页面加载完成后获取讲师列表
+  // pageData、 teacherQuery 变化时重新分页查询
   useEffect(() => {
-    getTeacherList()
+    pageTeacherListByCondition()
   }, [pageData, teacherQuery])
 
   // 分页器翻页的回调
@@ -76,13 +79,35 @@ const List = () => {
 
   // 讲师查询表单提交的回调
   const onSearch = (values: TeacherQuery) => {
+    const formData: TeacherQuery = { ...teacherQuery }
     // 收集表单数据
+    if (values.name) {
+      formData.name = values.name
+    }
+    if (values.level) {
+      formData.level = values.level
+    }
+    if (values.begin) {
+      formData.begin = dayjs(values.begin).format('YYYY-MM-DD HH:mm:ss')
+    }
+    if (values.end) {
+      formData.end = dayjs(values.end).format('YYYY-MM-DD HH:mm:ss')
+    }
+    // 更新查询条件
     setTeacherQuery({
       ...teacherQuery,
-      name: values.name,
-      level: values.level,
-      begin: dayjs(values.begin).format('YYYY-MM-DD'),
-      end: dayjs(values.end).format('YYYY-MM-DD'),
+      ...formData,
+    })
+  }
+
+  // 重置查询条件的回调
+  const onReset = () => {
+    // 重置查询条件
+    setTeacherQuery({
+      name: '',
+      level: 0,
+      begin: '',
+      end: '',
     })
   }
 
@@ -119,7 +144,12 @@ const List = () => {
           >
             查询
           </Button>
-          <Button type="default" icon={<RedoOutlined />} htmlType="reset">
+          <Button
+            type="default"
+            icon={<RedoOutlined />}
+            htmlType="reset"
+            onClick={onReset}
+          >
             重置
           </Button>
         </Form.Item>
