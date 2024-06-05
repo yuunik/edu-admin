e97fe6763ced0999d3dd7538048cc3bf4e378c9d
yuunik <yuunik_zhou@163.com>
2024-06-05 15:33:56 +0800

    feat: 配置 ant-design 中文包

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
+// ConfigProvider 用于全局配置国际化文案。
+import { ConfigProvider } from 'antd'
 // 引入路由器
 import router from '@/router'
 // 引入状态管理库
@@ -10,12 +12,17 @@ import store from '@/store'
 import 'virtual:svg-icons-register'
 // 引入全局样式
 import '@/styles/index.scss'
-// import './index.scss'
+// 引入 ant-design 汉化包
+import Chinese from 'antd/locale/zh_CN'
+// for date-picker i18n
+import 'dayjs/locale/zh-cn'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <ConfigProvider locale={Chinese}>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </ConfigProvider>
   </React.StrictMode>,
 )
